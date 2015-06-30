1178d844bbd907345cde81119ddbf6386c3ed829
Daniele Paganelli <d.paganelli@expertsystemsolutions.it>
2015-06-30 08:45:40 +0200

    minor fix in canondefine.sh

--- misura/canon/canondefine.sh
+++ misura/canon/canondefine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 T=`readlink -m "${BASH_SOURCE[0]}"`
-CANONROOT="$( cd "$( dirname "${T}" )"/.. && pwd )"
+CANONROOT="$( cd "$( dirname "${T}" )"/../.. && pwd )"
 export PYTHONPATH=$CANONROOT:$PYTHONPATH
