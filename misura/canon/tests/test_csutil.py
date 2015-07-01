ac4e1ed7b6a0450bb0df64a09ae28ab3d918251f
Riccardo Marotti <rmarotti@tainstruments.com>
2015-07-01 08:43:06 +0200

    minor

--- misura/canon/tests/test_csutil.py
+++ misura/canon/tests/test_csutil.py
@@ -5,7 +5,7 @@ from misura.canon.csutil import next_point
 
 class TestCsUtil(unittest.TestCase):
 	def test_next_point_only_strings(self):
-		curve = [[1, "curve", 2], [3, "with", 4], [5, "only", 6], [7, "strings", 8]]
+		curve = [[1, "curve", 2, 3], [3, "with", 4, 5], [5, "only", 6, 7], [7, "strings", 8, 9]]
 
 		actual_row_index, next_row = next_point(curve, 2)
 
@@ -21,12 +21,14 @@ class TestCsUtil(unittest.TestCase):
 		self.assertFalse(next_row)
 
 	def test_next_point(self):
-		curve = [['any value', 2, 'any value'], ['any value', 'not a number', 'any value'], ['any value', 456, 'any value']]
+		curve = [['any value', 2, 'any value', 'any value'], 
+				 ['any value', 'a string', 'any value', 'any value'], 
+				 ['any value', 456, 'any value', 'any value']]
 
 		actual_row_index, next_row = next_point(curve, 1)
 
 		self.assertEqual(actual_row_index, 2)
-		self.assertEqual(['any value', 456, 'any value'], next_row)
+		self.assertEqual(['any value', 456, 'any value', 'any value'], next_row)
 
 
 
