e14129b3ccc02f98531a750b75f6d116b8de231d
Daniele Paganelli <dpaganelli@tainstruments.com>
2019-02-28 22:23:55 +0100

    Simplified chron management, FLTD-673

--- misura/canon/option/common_proxy.py
+++ misura/canon/option/common_proxy.py
@@ -9,11 +9,13 @@ def manage_chron(obj, key, nval):
     if not obj.hasattr(key, 'chron'):
         obj.setattr(key, 'chron', [[],[]])
     chron = obj.getattr(key, 'chron')
-    chron[0].append(time())
-    chron[1].append(nval)
-    if len(chron[0])>2 and chron[0][-1]-chron[0][-2]<coalesce_chron:
-        chron[0].pop(-2)
-        chron[1].pop(-2)
+    t = time()
+    t -= t % coalesce_chron
+    if len(chron[0])>1 and t==chron[0][-1]:
+        chron[1][-1] = nval
+    else:
+        chron[0].append(t)
+        chron[1].append(nval)
     if len(chron)>20:
         chron[0].pop(0)
         chron[1].pop(0)
